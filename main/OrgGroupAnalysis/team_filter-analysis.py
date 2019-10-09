9cff06f30f5b5447cde83a83b6f6cc658553966f
Roy Yang <roy.j.yang@qut.edu.au>
2019-10-09 13:31:01 +1000

    update

--- main/OrgGroupAnalysis/team_filter-analysis.py
+++ main/OrgGroupAnalysis/team_filter-analysis.py
@@ -5,7 +5,6 @@ import sys
 sys.path.append('./src/')
 
 fn_event_log = sys.argv[1]
-#fn_report = sys.argv[2]
 
 if __name__ == '__main__':
     # Configuration based on given log (hard-coded)
@@ -17,21 +16,18 @@ if __name__ == '__main__':
         #el = read_disco_csv(f, mapping={'(case) channel': 6}) # bpic12 TODO
         #el = read_disco_csv(f, mapping={'(case) LoanGoal': 7}) # bpic17
 
-    print('Set K = ', end='')
-    num_groups = int(input())
-    #num_groups = list(range(3, 14))
+    num_groups = list(range(3, 17))
+
+    MAX_ITER = 2
 
     from ExecutionModeMiner.direct_groupby import ATonlyMiner, CTonlyMiner
     from ExecutionModeMiner.informed_groupby import TraceClusteringCTMiner
     from ResourceProfiler.raw_profiler import count_execution_frequency
     from OrganizationalModelMiner.clustering.hierarchical import _ahc
 
-    mode_miner = ATonlyMiner(el)
-    #mode_miner = CTonlyMiner(el, case_attr_name='(case) channel')
+    #mode_miner = ATonlyMiner(el)
+    mode_miner = CTonlyMiner(el, case_attr_name='(case) channel')
     #mode_miner = TraceClusteringCTMiner(el, fn_partition=fn_partition)
-
-    rl = mode_miner.derive_resource_log(el)
-    profiles = count_execution_frequency(rl, scale='log')
 
     '''
         Each of the particular selection of "k" is evaluated by both:
@@ -40,7 +36,8 @@ if __name__ == '__main__':
         - decision drawn from silhouette analysis, cf.
             (https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html)
     '''
-    ''' {7} items are collected for analysis:
+    ''' {10} items are collected for analysis:
+        - the result from silhouette analysis (k_flag),
         - the average value of silhouette score (of all resources), excluding
           those designated to singleton clusters;
         - the value from elbow method (cf. TODO) (of the whole clustering);
@@ -51,8 +48,11 @@ if __name__ == '__main__':
           singletons);
         - the number of clusters with NEG average silhouette score (excluding
           singletons);
+        - percentage of resources having zero/negative silhouette scores;
+        - percentage of cases involving the resouces above;
     '''
     items = [
+        'VALID_K_value',
         'average_silhouette_score_overall',
         'value_elbow_method',
         'num_singletons',
@@ -60,156 +60,101 @@ if __name__ == '__main__':
         'num_res_neg_silhouette_score',
         'num_clu_pos_silhouette_score',
         'num_clu_neg_silhouette_score',
+        'pct_resource_to_be_filtered',
+        'pct_case_to_be_filtered'
     ]
     from Evaluation.m2m.cluster_validation import silhouette_score
     from Evaluation.m2m.cluster_validation import variance_explained_percentage
     from numpy import mean, amin, amax
 
-    '''
-    max_gain = 0
-    max_after_filtered = 0
-    # Test: {
-    for k in num_groups:
-        ogs, _ = _ahc(profiles, k)
-        scores = silhouette_score(ogs, profiles)
-        mean_score_overall_old = mean(
-            list(scores.values()))
+    log = el
+    iteration = 1
+    while iteration <= MAX_ITER:
+        print('-' * 30 + 'Iteration: {}'.format(iteration) + '-' * 30)
 
-        #l_r_rm = set(r for r in scores if scores[r] == 0.0)
-        l_r_rm = set(r for r in scores if scores[r] <= 0.0)
-        print('\tFiltering out {} ({:.0%} of all {}) resources.'.format(
-            len(l_r_rm),
-            (len(l_r_rm) / len(set(el['resource']))),
-            len(set(el['resource']))))
-        del scores
+        rl = mode_miner.derive_resource_log(log)
+        profiles = count_execution_frequency(rl, scale='log')
+        l_measured_values = list()
+        for k in num_groups:
+            # calculate silhouette scores and variance explained (for each K)
+            ogs, _ = _ahc(profiles, k)
+            scores = silhouette_score(ogs, profiles)
+            var_pct = variance_explained_percentage(
+                ogs, profiles)
 
-        # Discovery (2): re-run on filtered log (discard cases)
-        l_case_rm = list()
-        for case_id, events in el.groupby('case_id'):
-            resources = set(events['resource'])
-            if len(l_r_rm.intersection(resources)) > 0:
-                l_case_rm.append(case_id)
-        print('\tFiltering out {} ({:.0%} of all {}) cases.'.format(
-            len(l_case_rm),
-            (len(l_case_rm) / len(set(el['case_id']))),
-            len(set(el['case_id']))))
+            # analyze the required items (for each K)
+            # object-level
+            mean_score_overall = mean(
+                list(scores.values()))
+            num_singleton_clusters = sum(
+                [1 for x in scores.values() if x == 0.0])
+            num_pos_score_objects = sum(
+                [1 for x in scores.values() if x > 0.0])
+            num_neg_score_objects = sum(
+                [1 for x in scores.values() if x < 0.0])
 
-        el_new = el[el['case_id'].map(lambda x: x not in l_case_rm)]
-        rl_new = mode_miner.derive_resource_log(el_new)
-        profiles_new = count_execution_frequency(rl_new, scale='log')
-        ogs_filtered, _ = _ahc(profiles_new, k)
-        scores = silhouette_score(ogs_filtered, profiles_new)
+            # cluster-level
+            scores_clu = list()
+            for g in ogs:
+                if len(g) > 1:
+                    score_g = mean([scores[r]
+                        for r in g if scores[r] != 0.0])
+                    max_score_g = amax([scores[r]
+                        for r in g if scores[r] != 0.0])
+                    scores_clu.append((score_g, max_score_g))
+            num_pos_score_clusters = sum(
+                [1 for x in scores_clu if x[0] > 0])
+            num_neg_score_clusters = sum(
+                [1 for x in scores_clu if x[0] <= 0])
+            k_flag = all(
+                [(x[1] >= mean_score_overall) for x in scores_clu])
 
-        # Analysis
-        mean_score_overall = mean(
-            #[x for x in scores.values() if x != 'nan'])
-            list(scores.values()))
-        max_after_filtered = (mean_score_overall 
-            if mean_score_overall > max_after_filtered else max_after_filtered)
+            # amount of resources, cases to be discarded (for each K)
+            l_r_rm = set(r for r in scores if scores[r] <= 0.0)
+            l_case_rm = list()
+            for case_id, events in log.groupby('case_id'):
+                resources = set(events['resource'])
+                if len(l_r_rm.intersection(resources)) > 0:
+                    l_case_rm.append(case_id)
+            pct_r_rm = len(l_r_rm) / len(set(log['resource']))
+            pct_case_rm = len(l_case_rm) / len(set(log['case_id']))
 
-        gain = mean_score_overall - mean_score_overall_old
-        print('K = {}:\t{}\t{}\t(delta = {})'.format(
-            k, True if gain > 0 else False, mean_score_overall, gain))
-        max_gain = gain if gain > max_gain else max_gain
-    print(max_gain)
-    print(max_after_filtered)
-    exit()
-    # }
-    '''
+            l_measured_values.append((
+                k_flag,
+                mean_score_overall,
+                var_pct,
+                num_singleton_clusters,
+                num_pos_score_objects,
+                num_neg_score_objects,
+                num_pos_score_clusters,
+                num_neg_score_clusters,
+                pct_r_rm,
+                pct_case_rm))
 
-    l_measured_values = list()
-    # Discovery (1): identify singletons and filter out
-    ogs, _ = _ahc(profiles, num_groups)
-    scores = silhouette_score(ogs, profiles)
-    print('{} to be filtered out:'.format(
-        sum([1 for x in scores.values() if x <= 0.0])))
-    for r, score in scores.items():
-        if score <= 0.0:
-            print('\t', end='')
-            print(r)
+        print('VALUES of K')
+        print(','.join(str(k) for k in num_groups))
+        print()
+        for i, attribute in enumerate(items): 
+            print(attribute)
+            print(','.join(str(l_measured_values[ik][i])
+                for ik, k in enumerate(num_groups)))
+            print()
 
-    #l_r_rm = set(r for r in scores if scores[r] == 0.0)
-    l_r_rm = set(r for r in scores if scores[r] <= 0.0)
-    del scores
+        # prompt for selecting a 'K'
+        if iteration + 1 <= MAX_ITER:
+            print('Select a "K" for the next iteration:\t', end='')
+            next_k = int(input())
+            ogs, _ = _ahc(profiles, next_k)
+            scores = silhouette_score(ogs, profiles)
+            l_r_rm = set(r for r in scores if scores[r] <= 0.0)
+            l_case_rm = list()
+            for case_id, events in log.groupby('case_id'):
+                resources = set(events['resource'])
+                if len(l_r_rm.intersection(resources)) > 0:
+                    l_case_rm.append(case_id)
+            # filter out cases and generate a new log
+            log = log[log['case_id'].map(lambda x: x not in l_case_rm)]
 
-    # Discovery (2): re-run on filtered log (discard cases)
-    l_case_rm = list()
-    for case_id, events in el.groupby('case_id'):
-        resources = set(events['resource'])
-        if len(l_r_rm.intersection(resources)) > 0:
-            l_case_rm.append(case_id)
-
-    print('\nFiltering out {} ({:.0%} of all {}) cases.'.format(
-        len(l_case_rm),
-        (len(l_case_rm) / len(set(el['case_id']))),
-        len(set(el['case_id']))))
-
-    el_good = el[el['case_id'].map(lambda x: x not in l_case_rm)]
-    rl_good = mode_miner.derive_resource_log(el_good)
-    profiles_good = count_execution_frequency(rl_good, scale='log')
-    ogs_filtered, _ = _ahc(profiles_good, num_groups)
-    scores = silhouette_score(ogs_filtered, profiles_good)
-    var_explained_pct = variance_explained_percentage(
-        ogs_filtered, profiles_good)
-
-    # Analysis
-    mean_score_overall = mean(
-        #[x for x in scores.values() if x != 'nan'])
-        list(scores.values()))
-    num_singleton_clusters = sum(
-        #[1 for x in scores.values() if x == 'nan'])
-        [1 for x in scores.values() if x == 0.0])
-    num_pos_score_objects = sum(
-        #[1 for x in scores.values() if x != 'nan' and x > 0.0])
-        [1 for x in scores.values() if x > 0.0])
-    num_neg_score_objects = sum(
-        #[1 for x in scores.values() if x != 'nan' and x <= 0.0])
-        [1 for x in scores.values() if x < 0.0])
-
-    scores_clu = list()
-    for g in ogs_filtered:
-        if len(g) > 1:
-            score_g = mean([scores[r] 
-                #for r in g if scores[r] != 'nan'])
-                for r in g if scores[r] != 0.0])
-            max_score_g = amax([scores[r]
-                #for r in g if scores[r] != 'nan'])
-                for r in g if scores[r] != 0.0])
-            scores_clu.append((score_g, max_score_g))
-    num_pos_score_clusters = sum(
-        [1 for x in scores_clu if x[0] > 0])
-    num_neg_score_clusters = sum(
-        [1 for x in scores_clu if x[0] <= 0])
-    k_flag = all(
-        [(x[1] >= mean_score_overall) for x in scores_clu])
-
-    print((
-        mean_score_overall,
-        var_explained_pct,
-        num_singleton_clusters,
-        num_pos_score_objects,
-        num_neg_score_objects,
-        num_pos_score_clusters,
-        num_neg_score_clusters,
-        k_flag
-    ))
-
-    del scores
-
-    el_worse = el[el['case_id'].map(lambda x: x in l_case_rm)]
-    #mode_miner = ATonlyMiner(el)
-    mode_miner = CTonlyMiner(el, case_attr_name='(case) channel')
-    rl_worse = mode_miner.derive_resource_log(el_worse)
-    profiles_worse = count_execution_frequency(rl_worse, scale='log')
-
-    num_groups = list(range(3, 14))
-    for k in num_groups:
-        ogs, _ = _ahc(profiles_worse, k)
-        scores = silhouette_score(ogs, profiles_worse)
-
-        # Analysis
-        mean_score_overall = mean(
-            #[x for x in scores.values() if x != 'nan'])
-            list(scores.values()))
-        print('K = {}:\t{}'.format(k, mean_score_overall))
+        # proceed to the next iteration
+        iteration += 1
 
