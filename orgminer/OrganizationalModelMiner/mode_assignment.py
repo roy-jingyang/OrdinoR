dacb98aff5b4fde716b6b77c6d85e56c1c22487a
roy-jingyang <7447643+roy-jingyang@users.noreply.github.com>
2020-01-26 15:18:57 +1000

    mode assignment methods updated, waiting for debug

--- orgminer/OrganizationalModelMiner/mode_assignment.py
+++ orgminer/OrganizationalModelMiner/mode_assignment.py
@@ -37,7 +37,7 @@ def full_recall(groups, rl):
 
 
 #TODO: Implementation #1 - OverallScore-WA
-def _overall_score(group, rl, p, w1=0.5, w2=0.5):
+def _overall_score(groups, rl, p, w1=0.5, w2=0.5):
     """Assign an execution mode to a group, as long as the overall score
     (as a weighted average) of its group relative stake and member
     coverage, is higher than a given threshold.
@@ -63,24 +63,70 @@ def _overall_score(group, rl, p, w1=0.5, w2=0.5):
     """
     print('Applying OverallScore with weights ({}, {}) '.format(w1, w2) +
         'and threshold {} '.format(p) + 'for mode assignment:')
-    tmp_modes = list()
     all_execution_modes = set(rl[['case_type', 'activity_type', 'time_type']]
         .drop_duplicates().itertuples(index=False, name=None))
 
     from orgminer.Evaluation.l2m.diagnostics import \
         group_relative_stake, member_coverage
+    from collections import defaultdict
 
-    for m in all_execution_modes:
-        rel_stake = group_relative_stake(group, m, rl)
-        cov = member_coverage(group, m, rl)
-        score = w1 * rel_stake + w2 * cov
-        if score > p:
-            tmp_modes.append((m, score))
+    scores_group_rel_stake = defaultdict(lambda: dict)
+    min_score_group_rel_stake = 1.0
+    max_score_group_rel_stake = 0.0
+    scores_group_cov = defaultdict(lambda: dict)
+    min_score_group_cov = 1.0
+    max_score_group_cov = 0.0
 
+    # obtain scores
+    for i, group in enumerate(groups):
+        for m in all_execution_modes:
+            rel_stake = group_relative_stake(group, m, rl)
+            scores_group_rel_stake[i][m] = rel_stake
+            min_score_group_rel_stake = rel_stake \
+                if rel_stake < min_score_group_rel_stake \
+                else min_score_group_rel_stake
+            max_score_group_rel_stake = rel_stake \
+                if rel_stake > max_score_group_rel_stake \
+                else max_score_group_rel_stake
+
+            cov = member_coverage(group, m, rl)
+            scores_group_cov[i][m] = cov
+            min_score_group_cov = cov \
+                if cov < min_score_group_cov \
+                else min_score_group_cov
+            max_score_group_cov = cov \
+                if cov > max_score_group_cov \
+                else max_score_group_cov
+
+    def min_max_scale(value, min_value, max_value):
+        return (value - min_value) / (max_value - min_value)
+    
+    # assign based on threshold filtering
     from operator import itemgetter
-    modes = list(item[0] 
-        for item in sorted(tmp_modes, key=itemgetter(1), reverse=True))
-    return modes
+    om = OrganizationalModel()
+    for i, group in enumerate(groups):
+        tmp_modes = list()
+        for m in all_execution_modes:
+            scaled_rel_stake = min_max_scale(
+                scores_group_rel_stake[i][m],
+                min_score_group_rel_stake,
+                max_score_group_rel_stake)
+            scaled_cov = min_max_scale(
+                scores_group_cov[i][m],
+                min_score_group_cov,
+                max_score_group_cov)
+
+            relatedness_score = (
+                w1 * scaled_rel_stake + 
+                w2 * scaled_cov
+            )
+            if relatedness_score > p:
+                tmp_modes.append((m, relatedness_score))
+        modes = list(item[0] 
+            for item in sorted(tmp_modes, key=itemgetter(1), reverse=True))
+        om.add_group(group, modes)
+
+    return om
 
 
 #TODO: Implementation #2 - OverallScore-HM
@@ -91,8 +137,8 @@ def overall_score(group, rl, p):
 
     Parameters
     ----------
-    group : iterator
-        Id of resources as a resource group.
+    groups : list of sets
+        Resource groups containing resource ids.
     rl : DataFrame
         A resource log.
     p : float
@@ -100,34 +146,74 @@ def overall_score(group, rl, p):
 
     Returns
     -------
-    modes : list of 3-tuples
-        Execution modes associated to the resource group, sorted by 
-        their relevance in terms of overall score from high to low.
+    om : OrganizationalModel
+        An organizational model resulted from assigning execution modes 
+        to resource groups.
     """
-    print('Applying OverallScore with ' +
-        'threshold {} '.format(p) + 'for mode assignment:')
-    tmp_modes = list()
+    print('Applying OverallScore with weights ({}, {}) '.format(w1, w2) +
+        'and threshold {} '.format(p) + 'for mode assignment:')
     all_execution_modes = set(rl[['case_type', 'activity_type', 'time_type']]
         .drop_duplicates().itertuples(index=False, name=None))
 
     from orgminer.Evaluation.l2m.diagnostics import \
-        group_relative_focus, group_relative_stake, member_coverage
+        group_relative_stake, member_coverage
+    from collections import defaultdict
 
-    for m in all_execution_modes:
-        #rel_stake = group_relative_stake(group, m, rl)
-        rel_focus = group_relative_focus(group, m, rl)
-        cov = member_coverage(group, m, rl)
-        #if rel_stake > 0 or cov > 0:
-        if rel_focus > 0 or cov > 0:
-            #score = 2 * rel_stake * cov / (rel_stake + cov)
-            score = 2 * rel_focus * cov / (rel_focus + cov)
-            if score > p:
-                tmp_modes.append((m, score))
-        else:
-            pass
+    scores_group_rel_stake = defaultdict(lambda: dict)
+    min_score_group_rel_stake = 1.0
+    max_score_group_rel_stake = 0.0
+    scores_group_cov = defaultdict(lambda: dict)
+    min_score_group_cov = 1.0
+    max_score_group_cov = 0.0
 
+    # obtain scores
+    for i, group in enumerate(groups):
+        for m in all_execution_modes:
+            rel_stake = group_relative_stake(group, m, rl)
+            scores_group_rel_stake[i][m] = rel_stake
+            min_score_group_rel_stake = rel_stake \
+                if rel_stake < min_score_group_rel_stake \
+                else min_score_group_rel_stake
+            max_score_group_rel_stake = rel_stake \
+                if rel_stake > max_score_group_rel_stake \
+                else max_score_group_rel_stake
+
+            cov = member_coverage(group, m, rl)
+            scores_group_cov[i][m] = cov
+            min_score_group_cov = cov \
+                if cov < min_score_group_cov \
+                else min_score_group_cov
+            max_score_group_cov = cov \
+                if cov > max_score_group_cov \
+                else max_score_group_cov
+
+    def min_max_scale(value, min_value, max_value):
+        return (value - min_value) / (max_value - min_value)
+    
+    # assign based on threshold filtering
     from operator import itemgetter
-    modes = list(item[0] 
-        for item in sorted(tmp_modes, key=itemgetter(1), reverse=True))
-    return modes
+    om = OrganizationalModel()
+    for i, group in enumerate(groups):
+        tmp_modes = list()
+        for m in all_execution_modes:
+            scaled_rel_stake = min_max_scale(
+                scores_group_rel_stake[i][m],
+                min_score_group_rel_stake,
+                max_score_group_rel_stake)
+            scaled_cov = min_max_scale(
+                scores_group_cov[i][m],
+                min_score_group_cov,
+                max_score_group_cov)
+
+            relatedness_score = (
+                2 * (scaled_rel_stake * scaled_cov) /
+                (scaled_rel_stake + scaled_cov)
+            )
+            if relatedness_score > p:
+                tmp_modes.append((m, relatedness_score))
+        modes = list(item[0] 
+            for item in sorted(tmp_modes, key=itemgetter(1), reverse=True))
+        om.add_group(group, modes)
+
+    return om
 
