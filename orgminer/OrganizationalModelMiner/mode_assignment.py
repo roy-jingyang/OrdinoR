7058e9b2261f1cdeb5577c32b96d50ee4b7f16f4
roy-jingyang <7447643+roy-jingyang@users.noreply.github.com>
2020-02-07 11:47:15 +1000

    update grid search util

--- orgminer/OrganizationalModelMiner/mode_assignment.py
+++ orgminer/OrganizationalModelMiner/mode_assignment.py
@@ -86,7 +86,7 @@ def overall_score(groups, rl, p, w1=0.5, w2=None, auto_search=False):
         solution, params = grid_search(
             partial(overall_score, groups=groups, rl=rl), 
             params_config={
-            'p': list(x / 10 for x in range(2, 8)), # TODO
+            'p': list(x / 10 for x in range(1, 11)),
             'w1': list(x / 10 for x in range(11))},
             func_eval_score=f1_score
         )
