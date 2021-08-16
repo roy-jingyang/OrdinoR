0c3d62650074eec016263a6b1c433faf43dfe7a1
roy-jingyang <7447643+roy-jingyang@users.noreply.github.com>
2021-08-16 22:36:30 +1000

    0.1.0rc11 ready to test

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ py_ver = sys.version_info[:2]
 if py_ver < (3, 7) or py_ver > (3, 8):
     sys.stderr.write(
         f"""
-        OrdinoR requires Python 3.7, or 3.8.
+        OrdinoR requires Python 3.7 or 3.8.
         You have Python {py_ver[0]}.{py_ver[1]}.\n
         """
     )
@@ -41,16 +41,21 @@ classifier      = [
 
 # Package information
 name                = 'ordinor'
-version             = '0.1.0rc10'
+version             = '0.1.0rc11'
 python_requires     = '>=3.7, <=3.8'
 
 packages            = [
     'ordinor',
     'ordinor.analysis',
+    'ordinor.analysis.group_profiles',
+    'ordinor.analysis.m2m',
     'ordinor.conformance',
     'ordinor.execution_context',
     'ordinor.io',
     'ordinor.org_model_miner',
+    'ordinor.org_model_miner.clustering',
+    'ordinor.org_model_miner.community',
+    'ordinor.org_model_miner.models',
     'ordinor.social_network_miner',
     'ordinor.utils',
 ]
@@ -62,9 +67,23 @@ def parse_requirements_file(filename):
 
 install_requires    = []
 extras_require      = {
-    "default": parse_requirements_file('./requirements.txt')
-}
+    'default': {
+        'numpy>=1.17.2',
+        'scipy>=1.3.1',
+        'scikit-learn>=0.21.3',
+        'scikit-fuzzy>=0.4.1',
+        'pandas>=0.25.3',
+        'networkx>=2.4',
+        'pm4py>=1.2.4',
 
+        # webapp requirements
+        'Flask',
+        'flask-cors',
+        'flask-bootstrap',
+        'flask-wtf',
+        'flask-session',
+    }
+}
 
 if __name__ == '__main__':
     setuptools.setup(
@@ -80,6 +99,5 @@ if __name__ == '__main__':
         python_requires=python_requires,
         install_requires=install_requires,
         extras_require=extras_require,
-
-        #TODO
+        packages=packages,
     )
